0bdacdf7f83992c1071b5d6566690a3440e8e4b8
HernanLn <laura.hernandez@perkinelmer.com>
2023-06-12 22:34:34 +0200

    Add main.py

--- main.py
+++ main.py
@@ -5,4 +5,3 @@ import scipy.stats as stats
 df = pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/blood_pressure.csv")
 df.info()
 results = stats.ttest_ind(df['bp_after'][df['sex'] == 'Male'], df['bp_after'][df['sex'] == 'Female'])
-print (results)
